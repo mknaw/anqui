2a8489026633b007095f3cce4118a0f0467c9c5b
mknaw <mknawara@gmail.com>
2022-04-06 21:23:15 -0400

    limited for free heroku

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,11 +1,15 @@
 use diesel::pg::PgConnection;
-use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::r2d2::{Builder, ConnectionManager, Pool};
 use std::env;
 
 pub type DbPool = Pool<ConnectionManager<PgConnection>>;
 
 pub fn new_db_pool() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set.");
+    let max_db_connections = env::var("MAX_DB_CONNECTIONS")
+        .expect("MAX_DB_CONNECTIONS not set.")
+        .parse()
+        .unwrap();
     let manager = ConnectionManager::<PgConnection>::new(database_url);
-    Pool::new(manager).unwrap()
+    Builder::new().max_size(max_db_connections).build_unchecked(manager)
 }
